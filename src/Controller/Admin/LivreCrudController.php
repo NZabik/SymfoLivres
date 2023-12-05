af03f7a245fe29b2cb38d45aa9c19dd291a53c3a
Nicolas Zabik <melchiah.nico@gmail.com>
2023-12-05 18:05:04 +0100

    location en cours

--- src/Controller/Admin/LivreCrudController.php
+++ src/Controller/Admin/LivreCrudController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Livre;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class LivreCrudController extends AbstractCrudController
 {
@@ -15,14 +17,18 @@ class LivreCrudController extends AbstractCrudController
         return Livre::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('nom'),
+            TextEditorField::new('resume'),
+            Field::new('couverture'),
+            Field::new('auteur.nom'),
+            Field::new('editeur.nom'),
+            Field::new('user.id'),
         ];
     }
-    */
+    
 }
