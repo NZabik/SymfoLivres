f5e3a8d54332414b075c0f0e8ee458e3c2c79b02
Nicolas Zabik <melchiah.nico@gmail.com>
2023-12-05 09:13:12 +0100

    login, register,modification infos et mot de passe, cards livres opérationnels

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -91,29 +92,27 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
         
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => [
-                    'autocomplete' => 'new-password',
-                    'class' => 'form-control'
+            ->add('plainPassword', RepeatedType::class,[
+                'type'=>PasswordType::class,
+                'first_options'=>[
+                    'attr' => [
+                        'class' => 'form-control',
+                    ],
+                    'label'=>'Mot de passe',
+                    'label_attr' => [
+                        'class' => 'form-label mt-4',
+                    ],
                 ],
-                'label' => 'Mot de passe',
-                'label_attr' => [
-                    'class' => 'form-label mt-4',
+                'second_options'=>[
+                    'attr' => [
+                        'class' => 'form-control',
+                    ],
+                    'label'=>'Confirmation du mot de passe',
+                    'label_attr' => [
+                        'class' => 'form-label mt-4',
+                    ],
                 ],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
+                'invalid_message'=>'les mots de passe ne correspondent pas'
             ]);
     }
 
